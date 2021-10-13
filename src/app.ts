d8a0dd42666c670ae54e3695319923e157f0dba2
Daniiar <daniyar.alymseitov@Gmail.com>
2021-10-13 23:42:06 +0600

    Puppeter added, ts added

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello')
+import puppeteer from 'puppeteer';
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com/s?k=lord+of+ring+book&ref=nb_sb_noss_2');
+    const link = await page.$$('.a-link-normal');
+    await link[0].click();
+    await page.screenshot({ path: 'huh.jpg' });
+
+    await browser.close();
+})();
